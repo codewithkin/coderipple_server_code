631bdd3676defd439eb1e7312a57fc0e1184dad0
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2024-12-31 09:05:45 +0200

    update uploadFile.js

--- code/lib/uploadFile.js
+++ code/lib/uploadFile.js
@@ -1,12 +1,13 @@
 import { uploadDirect } from '@uploadcare/upload-client'
 
-export default async function UploadFile(fileBlob) {
+export default async function UploadFile(fileBlob, options) {
   // fileData must be `Blob` or `File` or `Buffer`
   const result = await uploadDirect(
     fileBlob,
     {
       publicKey: "07467f541ac1cffdd4a4",
       store: 'auto',
+      fileName: options.name
     }
   )
 
